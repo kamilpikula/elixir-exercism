8b7da73da69e602e0a7adbbb090c0cb4808a064a
kamilpikula <pikusix1@o2.pl>
2020-03-16 00:27:34 +0100

    implement main function

--- twelve-days/lib/twelve_days.ex
+++ twelve-days/lib/twelve_days.ex
@@ -1,10 +1,33 @@
 defmodule TwelveDays do
+  @days %{
+    1 => {:first, "and a Partridge in a Pear Tree."},
+    2 => {:second, "two Turtle Doves"},
+    3 => {:third, "three French Hens"},
+    4 => {:fourth, "four Calling Birds"},
+    5 => {:fifth, "five Gold Rings"},
+    6 => {:sixth, "six Geese-a-Laying"},
+    7 => {:seventh, "seven Swans-a-Swimming"},
+    8 => {:eight, "eight Maids-a-Milking"},
+    9 => {:ninth, "nine Ladies Dancing"},
+    10 => {:tenth, "ten Lords-a-Leaping"},
+    11 => {:eleventh, "eleven Pipers Piping"},
+    12 => {:twelfth, "twelve Drummers Drumming"}
+  }
   @doc """
   Given a `number`, return the song's verse for that specific day, including
   all gifts for previous days in the same line.
   """
   @spec verse(number :: integer) :: String.t()
-  def verse(number) do
+  def verse(1) do
+    "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+  end
+
+  def verse(number) when number >= 1 do
+    {numerator, gift} = Map.get(@days, number)
+    Enum.take(@days, number - 1)
+    |> Enum.reverse()
+    |> Enum.reduce("On the #{numerator} day of Christmas my true love gave to me: #{gift}", fn {_number, {_, gift_day}}, acc -> acc <> ", #{gift_day}" end)
+    |> String.trim()
   end
 
   @doc """
