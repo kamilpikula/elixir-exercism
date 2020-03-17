41efed960472b3c43e54aef3705d6b0902f16034
kamilpikula <pikusix1@o2.pl>
2020-03-17 22:44:41 +0100

    anagram

---
+++ anagram/lib/anagram.ex
@@ -0,0 +1,20 @@
+defmodule Anagram do
+  @doc """
+  Returns all candidates that are anagrams of, but not equal to, 'base'.
+  """
+  @spec match(String.t(), [String.t()]) :: [String.t()]
+  def match(base, candidates) do
+    Enum.filter(candidates, fn word ->
+      sort_string(base) == sort_string(word) end)
+    |> Enum.reject(fn word ->
+       String.downcase(base) == String.downcase(word)
+    end)
+  end
+
+  def sort_string(string) do
+    string
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.sort()
+  end
+end
