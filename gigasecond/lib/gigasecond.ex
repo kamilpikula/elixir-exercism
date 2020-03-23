08388a05cf9eae4ae1994080e7643d8cce59753b
kamilpikula <pikusix1@o2.pl>
2020-03-23 23:09:50 +0100

    refactoring v2

--- gigasecond/lib/gigasecond.ex
+++ gigasecond/lib/gigasecond.ex
@@ -1,16 +1,14 @@
 defmodule Gigasecond do
-  @gigasecond 1_000_000_000
   @doc """
   Calculate a date one billion seconds after an input date.
   """
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           :calendar.datetime()
 
-  def from({{year, month, day}, {hours, minutes, seconds}}) do
-    {:ok, from_date} = NaiveDateTime.from_erl({{year, month, day}, {hours, minutes, seconds}})
-
-    new_time = NaiveDateTime.add(from_date, @gigasecond)
-
-    NaiveDateTime.to_erl(new_time)
+  def from({{_year, _month, _day}, {_hours, _minutes, _seconds}} = date) do
+    date
+    |> NaiveDateTime.from_erl!()
+    |> NaiveDateTime.add(1_000_000_000)
+    |> NaiveDateTime.to_erl()
   end
 end
